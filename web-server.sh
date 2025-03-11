54417ce92bc48705fa3a938d391faf7b4164b41d
Adesokan segun <sheezycouture@gmail.com>
2025-03-11 22:37:51 +0100

    added a github action yaml file

--- web-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### updating centos server using yum ############
-sudo yum update
-sudo yum install httpd -y 
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl status httpd
-
